43c63c539f1e84a5a74d040b26de1089409e7132
David O'Leary <david.oleary174@gmail.com>
2017-01-10 15:28:46 +0000

    Added image to browser for each character

--- client/src/components/CharacterDetail.jsx
+++ client/src/components/CharacterDetail.jsx
@@ -8,6 +8,8 @@ var CharacterDetail = function (props) {
    <div className='character-detail'>
    <h4>{props.character.name}</h4>
    <p>House: {props.character.house}</p>
+   <p>Species: {props.character.species}</p>
+   <img src={props.character.image}/>
    </div>
   );
 };
